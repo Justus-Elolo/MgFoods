c2d0c5948c817ed4470ae50964ba1e996b27a5db
Justus-Elolo <jasimrootie@gmail.com>
2024-05-24 11:38:44 +0300

    Frontend

---
+++ includes/front/top.php
@@ -0,0 +1,30 @@
+<?php require 'includes/config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Mg Foods</title>
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<meta content="" name="BigFish Restaurant Nairobi Kenya">
+	<meta content="" name="Online Food Order Processing Website for BigFish Restaurant Nairobi">
+
+	<!-- Favicons -->
+	<link href="img/logo/mgfoodhavenlogo2.jpg" rel="icon">
+	<link href="img/logo/mgfoodhavenlogo2.jpg" rel="apple-touch-icon">
+
+	<!-- Google Fonts -->
+	<link href="css/fonts.css" rel="stylesheet">
+
+	<!-- Bootstrap CSS File -->
+	<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Libraries CSS Files -->
+	<link href="admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">	
+	<link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="lib/animate/animate.min.css" rel="stylesheet">
+	<link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+	<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+	<link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+	<!-- Main Stylesheet File -->
+	<link href="css/style.css" rel="stylesheet">
